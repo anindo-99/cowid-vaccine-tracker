ce983a1f554d4c7f5353322dbf3780159d9fdeb8
Anindo Maumdar <anindo.1999maz@gmail.com>
2021-05-26 17:51:35 +0530

    First Push

---
+++ tracker/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Userprofile(models.Model):
+    user = models.OneToOneField(
+        User, primary_key=True, blank=True, on_delete=models.CASCADE)
+    # profile_pic = models.ImageField(
+    #     upload_to='images', default='default/user.png')
+    # first_name = models.CharField(max_length=100, null=True, blank=True)
+    # last_name = models.CharField(max_length=100, null=True, blank=True)
+
+    phone = models.CharField(max_length=10, null=True, blank=True)
+    auth_token = models.CharField(max_length=100)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user.first_name)
---
+++ tracker/urls.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+from . import views
+# from.views import CustomLoginView
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    # path('accounts/login/', CustomLoginView.as_view(), name='login'),
+    path('accounts/login/', views.login_attempt, name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    # path('login/', views.loginPage, name="login"),
+    # path('handlogin/', views.handleLogin, name="handlelogin"),
+
+    path('register/', views.registerPage, name="register"),
+    path('handleregister/', views.handleRegister, name="handleregister"),
+    path('verify/<auth_token>', views.verify, name="verify"),
+]
